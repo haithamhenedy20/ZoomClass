0fad5bd5d4d4fc94f6a8050d831461f397ac00e3
haithamhenedy18A# <haitham.m.henedy@gmail.com>
2021-11-12 16:07:17 -0500

    my 2nd commit after ZULKAR Changes

--- JavaClass/src/myStudyInSequence/TheClassAddedByZulkar.java
+++ JavaClass/src/myStudyInSequence/TheClassAddedByZulkar.java
@@ -4,5 +4,7 @@ public class TheClassAddedByZulkar {
 	
 	public static void main(String[] args) {
 		System.out.println("The first class added by zulkar");
+		
+		//Just A trial to check GIT/GITHUB
 	}
 }
